1b5b38c562662f4f65cda18161c9fe50418b8138
Oluwafemi Peter Oderinwale <oderinwalefemi150@gmail.com>
2025-08-07 10:48:42 +0100

    Added JWT class

--- app/core/JWT.php
+++ app/core/JWT.php
@@ -5,16 +5,17 @@ use Firebase\JWT\JWT as FirebaseJWT;
 use Firebase\JWT\Key;
 
 class JWT {
+    private static $key = JWT_SECRET;
+
     public static function create($payload) {
-        $key = JWT_SECRET;
         $payload['iat'] = time();
         $payload['exp'] = time() + 3600;
-        return FirebaseJWT::encode($payload, $key, 'HS256');
+        return FirebaseJWT::encode($payload, self::$key, 'HS256');
     }
 
     public static function verify($token) {
         try {
-            return FirebaseJWT::decode($token, new Key(JWT_SECRET, 'HS256'));
+            return FirebaseJWT::decode($token, new Key(self::$key, 'HS256'));
         } catch (\Exception $e) {
             return null;
         }
